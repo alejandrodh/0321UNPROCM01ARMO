efab366a900109be4c1fa7cae4b5cdcb8b253f35
ale-vivone <vivoneaj@gmail.com>
2021-04-15 15:18:36 -0300

    Todo el material de clase hasta la 1ra de Javascript.

---
+++ clase05_consolaYGIT/readme.md
@@ -0,0 +1,7 @@
+## Temas de la clase
+- ¿Qué es y para que nos sirve la terminal
+- Cómando de consola más usados.
+
+## GIT
+Será nuestra herramienta para compartir código. 
+
---
+++ clase11_introJs/js/index.js
@@ -0,0 +1,17 @@
+console.log("Hola. ¿Cómo están?");
+
+//Asignando y reasignando con let
+let nombre = "Ale";
+nombre = "Maca";
+console.log(nombre);
+
+//Asignando y reasignando con let
+const universidad = "UdeSA";
+// universidad = "UCA";
+
+// tipos de datos
+let numero = 12312312;
+let teGustaElHelado = true;
+
+// Template literal
+console.log(`Hola mi nombres Octavio y voy a ${universidad}`)
